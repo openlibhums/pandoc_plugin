f439a665379bd7fd9a3018bfdea90a789e4ace12
Andy Byers <andy.byers@openlibhums.org>
2018-07-10 16:11:02 +0100

    Updated plugin_settings, short_name should = module_name.

--- plugin_settings.py
+++ plugin_settings.py
@@ -4,8 +4,9 @@ PLUGIN_NAME = 'Pandoc Plugin'
 DESCRIPTION = 'A plugin to assist typesetters with converting docx/rtf files to html or xml'
 AUTHOR = 'Drew Stimson and Daniel Evans'
 VERSION = 0.1
-SHORT_NAME = 'pandoc'
+SHORT_NAME = 'pandoc_plugin'
 MANAGER_URL = 'pandoc_index'
+
 
 def install():
     new_plugin, created = models.Plugin.objects.get_or_create(name=SHORT_NAME, version=VERSION, enabled=True)
@@ -15,12 +16,15 @@ def install():
     else:
         print('Plugin {0} is already installed.'.format(PLUGIN_NAME))
 
-    models.PluginSetting.objects.get_or_create(name='pandoc_enabled', plugin=new_plugin, types='boolean',
-                                               pretty_name='Enable Pandoc Plugin', description='Enable Pandoc Conversion Plugin',
+    models.PluginSetting.objects.get_or_create(name='pandoc_enabled',
+                                               plugin=new_plugin,
+                                               types='boolean',
+                                               pretty_name='Enable Pandoc Plugin',
+                                               description='Enable Pandoc Conversion Plugin',
                                                is_translatable=False)
 
 def hook_registry():
-    '''
+    """
     When site with hooks loaded, this is run for each plugin to create list of plugins
-    '''
+    """
     return {'conversion_buttons': {'module': 'plugins.pandoc_plugin.hooks', 'function': 'inject_pandoc'}}
