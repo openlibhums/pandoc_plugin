fbfe8436b50f8c45a5708fe563c91b1422adb350
Andy Byers <andy.byers@openlibhums.org>
2018-07-10 16:10:40 +0100

    Tweaked hooks.

--- hooks.py
+++ hooks.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
-from django.urls import reverse
 
 from plugins.pandoc_plugin import plugin_settings
 from utils import models, setting_handler
 
+
 def inject_pandoc(context):
-    '''
+    """
     Provides buttons for users to automatically convert manuscript files (docx or rtf) to html or xml.
     Uses the pandoc plugin, which must be installed on the server.
-    '''
+    """
     plugin = models.Plugin.objects.get(name=plugin_settings.SHORT_NAME)
     request = context.get('request')
-    pandoc_enabled = setting_handler.get_plugin_setting(plugin, 'pandoc_enabled', request.journal)
+    pandoc_enabled = setting_handler.get_plugin_setting(plugin, 'pandoc_enabled', request.journal, create=True,
+                                                        pretty='Pandoc Enabled', types='boolean')
 
     if not pandoc_enabled.value:
         return ''
