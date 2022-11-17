187418de9e33e9e87654bbe023e3ca80b5d99c0d
Andy Byers <a.byers@bbk.ac.uk>
2022-11-17 12:36:30 +0000

    Plugin now runs on Django 3.2

--- urls.py
+++ urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from plugins.pandoc_plugin import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='pandoc_index'),
-    url(r'^convert/(?P<article_id>\d+)/file/(?P<file_id>\d+)/$', views.convert_file, name='pandoc_convert'),
+    re_path(r'^$', views.index, name='pandoc_index'),
+    re_path(r'^convert/(?P<article_id>\d+)/file/(?P<file_id>\d+)/$', views.convert_file, name='pandoc_convert'),
 ]
